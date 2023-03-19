63bc1c5000928aaa67f162c6517d4790d4b9ad07
Jinchen2028 <2289908757@qq.com>
2023-03-19 22:42:54 +0800

    添加了factsheet

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=0 python test_demo.py --data_dir /home/thor/projects/data/NTIRE2022_Challenge --save_dir /home/thor/projects/data/NTIRE2023_Challenge/results --model_id 0
+CUDA_VISIBLE_DEVICES=0 python test_demo.py --data_dir /root/autodl-tmp/DIV2K_val_100 --save_dir . --model_id 3
 
